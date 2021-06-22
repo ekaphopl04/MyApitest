a2ad6a066e3494e388cb2b43244f17c8fb974928
ekaphopl04 <ekaphop.l04@gmail.com>
2021-06-22 15:11:36 +0700

    Apitest

---
+++ Controllers/Valuemodel.cs
@@ -0,0 +1,15 @@
+namespace HelloWebApi{
+
+ public class Itemsend{
+     public string Id {get ; set ; } 
+     public string Name {get ; set ; } 
+     public string Surname {get ; set ; } 
+
+ }
+ public class Itemreturn{
+
+     public string Id {get ; set ;}
+    public string Fullname {get ; set ;}
+ }
+
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet("{id}")]
+        public int GetName(int id){
+           return id ; 
+        }
+        [HttpPost()]
+        public string Post(string value)
+        {
+            return value;
+
+        }
+        [HttpPut()]
+        public string Put(string value)
+        {
+            return value;
+
+        }
+        [HttpPost()]
+        public Itemreturn Sending(Itemsend Item){
+
+            var Fullname = Item.Name+""+Item.Surname ;
+            return new Itemreturn{
+                Id = Item.Id,
+                Fullname = Fullname 
+            };
+        }
+
+    }
+}
